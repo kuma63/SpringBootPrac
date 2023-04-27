508bc7b9342c5d4adca357d4671ab46b48ef4097
kuma63 <t09min25.y@gmail.com>
2023-04-27 18:00:51 +0900

    [finish]chap12_SizeValidation

--- src/main/java/com/example/sample3app/beans/SearchForm.java
+++ src/main/java/com/example/sample3app/beans/SearchForm.java
@@ -1,5 +1,7 @@
 package com.example.sample3app.beans;
 
+import javax.validation.constraints.Size;
+
 import lombok.Data;
 
 /*
@@ -10,5 +12,6 @@ public class SearchForm {
 	//ブランドID
 	private String brandId;
 	//キーワード
+	@Size(min=2, max=10)
 	private String keyword;
 }
