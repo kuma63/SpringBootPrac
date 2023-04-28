f195e3e024fe9cd78b65f0d9c533f32e12c34f1f
kuma63 <t09min25.y@gmail.com>
2023-04-28 14:37:00 +0900

    [finish]chap16更新処理ロールバック実装

--- src/main/java/com/example/sample3app/services/MotosService.java
+++ src/main/java/com/example/sample3app/services/MotosService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.OptimisticLockingFailureException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.sample3app.beans.Brand;
 import com.example.sample3app.beans.Motorcycle;
@@ -39,11 +40,17 @@ public class MotosService {
      * @param moto バイク情報
      * @return 更新件数
      */
+    @Transactional
 	public int save(Motorcycle moto) {
 		int cnt = mapper.update(moto);
 		if(cnt == 0) {
 			throw new OptimisticLockingFailureException("楽観的排他エラー");
 		}
+		
+		// 2件以上更新は想定外（SQLの不備の可能性）
+	    if (cnt > 1) {
+	        throw new RuntimeException("2件以上更新されました。");
+	    }
 		return cnt;
 		
 	}
